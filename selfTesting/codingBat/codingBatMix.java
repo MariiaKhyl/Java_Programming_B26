a76634b343c1c42cefed7c47ed17cf59e30266dc
mashk <mashkaxil@gmail.com>
2022-05-12 14:47:11 -0500

    day_55

--- selfTesting/codingBat/codingBatMix.java
+++ selfTesting/codingBat/codingBatMix.java
@@ -1,4 +1,4 @@
-package more;
+package codingBat;
 
 public class codingBatMix {
 /*
--- selfTesting/replit/ZombieAttack2.java
+++ selfTesting/replit/ZombieAttack2.java
@@ -1,12 +1,62 @@
 package replit;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ZombieAttack2 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+     /*   Scanner input = new Scanner(System.in);
         int[] inhabitants = {input.nextInt(), input.nextInt(), input.nextInt(), input.nextInt(), input.nextInt(), input.nextInt(), input.nextInt(), input.nextInt()};
+        int[] zero = new int[arr.length];
+        boolean run = true;
+        int day = 0;
 
+        System.out.println("Day " + day++ + " " + Arrays.toString(arr));
+
+        while(run) {
+            for(int i = 0; i < arr.length; i++) {
+                if(arr[i] != 0) {
+                    run = true;
+                    break;
+                } else {
+                    run = false;
+                }
+            }
+
+            if(!run) {
+                break;
+            }
+
+            for(int i = 0; i < arr.length; i++) {
+                if(arr[i] == 0) {
+                    zero[i] = -1;
+                }
+            }
+
+            for(int i = 0; i < zero.length; i++) {
+                if(i == 0) {
+                    if(zero[i + 1] == -1) {
+                        int x = arr[i];
+                        arr[i] = x / 2;
+                    }
+                } else if(i == zero.length - 1) {
+                    if(zero[i - 1] == -1) {
+                        int x = arr[i];
+                        arr[i] = x / 2;
+                    }
+                } else if(zero[i - 1] == -1 || zero[i + 1] == -1) {
+                    int x = arr[i];
+                    arr[i] = x / 2;
+                }
+            }
+            System.out.println("Day " + day++ + " " + Arrays.toString(arr));
+            run = true;
+        }
+        System.out.println("---- EXTINCT ----");
+    }
+}
+
+      */
 
     }
 }
